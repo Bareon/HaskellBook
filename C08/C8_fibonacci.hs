daeda0a447cfcc8773bf7530b324311adff2e1a9
Brent <scheppmann@gmail.com>
2017-10-12 13:28:47 -0700

    Update C8_fibonacci.hs

--- C08/C8_fibonacci.hs
+++ C08/C8_fibonacci.hs
@@ -4,6 +4,9 @@ fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci x = fibonacci (x -1) + fibonacci (x -2)
 
+-- zipWith
+fib = zipWith (+) (0:(1:fib)) (0:fib)
+--print $ take 100 fib
 
 dividedBy :: Integral a => a -> a -> (a,a)
 dividedBy num denom = go num denom 0
